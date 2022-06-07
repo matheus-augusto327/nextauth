41b04eb5ae3e7ec44d5f143a36ba929fa41b3c9b
Matheus <matheus.augustoreal@gmail.com>
2022-06-07 05:27:37 -0300

    Broadcast logout

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react"
 import { Can } from "../components/Can"
-import { AuthContext } from "../contexts/AuthContext"
+import { AuthContext, signOut } from "../contexts/AuthContext"
 import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
@@ -17,6 +17,8 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign out</button>
 
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
